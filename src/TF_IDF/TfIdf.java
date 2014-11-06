63f618613011af6671d57a7e6a03522728da9110
Niall <Niall@Niall-HP.lan>
2014-11-06 22:10:20 +0000

    Tfidf update 1

--- src/TF_IDF/TfIdf.java
+++ src/TF_IDF/TfIdf.java
@@ -3,21 +3,36 @@ package TF_IDF;
  *
  * @author Niall
  */
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class TfIdf { // /** * Calculated the tf of term termToCheck * 
 //@param totalterms : Array of all the words under processing document * @param termToCheck : term of which tf is to be calculated. * @return tf(term frequency) of term termToCheck */ 
+    static Map<String, Map<String, Integer>> output = new HashMap<String, Map<String, Integer>>();
     
+    public TfIdf(Map<String, Map<String, Integer>> output){
+        this.output=output;
+    }
 //    TF(t) = (Number of times term t appears in a document) / (Total number of terms in the document)
-    public static double tfCalculator(String[] totalterms, String termToCheck) { 
-        double count = 0; //to count the overall occurrence of the term termToCheck 
-        for (String s : totalterms) { 
-            if (s.equalsIgnoreCase(termToCheck)) { 
-                count++; 
-            } 
-        } 
+    public double tfCalculator(String term, int docLength) {
+//        double count = 0; //to count the overall occurrence of the term termToCheck 
+//        for (String s : totalterms) { 
+//            if (s.equalsIgnoreCase(termToCheck)) { 
+//                count++; 
+//            } 
+//        }
+        
+        Set<Map.Entry<String, Integer>> entries = output.get(term).entrySet();
+        String[] amountOfTermInDocument = null;
+        for (Map.Entry entry : entries) {
+            amountOfTermInDocument = entry.toString().split("=");
+            System.out.println("entry: "+Arrays.toString(amountOfTermInDocument));
+        }
 
-        return count / totalterms.length; 
+        return Integer.parseInt(amountOfTermInDocument[1]) / docLength; 
     } // // /** * Calculated idf of term termToCheck * @param allTerms : all the terms of all the documents * @param termToCheck * @return idf(inverse document frequency) score */ 
 
 //    IDF(t) = log(Total number of documents / Number of documents with term t in it).
